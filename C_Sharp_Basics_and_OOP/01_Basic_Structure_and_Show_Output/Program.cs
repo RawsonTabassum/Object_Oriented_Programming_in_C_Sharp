f2d964fbe3c246a8194723fbf0b8e258f3b43936
RawsonTabassum <rawson17113@gmail.com>
2024-09-11 14:10:45 +0600

    Basic Structure and Show Output Updated

--- C_Sharp_Basics_and_OOP/01_Basic_Structure_and_Show_Output/Program.cs
+++ C_Sharp_Basics_and_OOP/01_Basic_Structure_and_Show_Output/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// This is the basic structure of a C# program
+// System is a namespace. 
+// using system; imports the namespace and you can now access the classes in the System namespace
+// Program is a class which contains the Main() function
+// The Main() function is the starting point of a C# program. A C# program can only have one Main() function.
+// Console.WriteLine() is the output function. Here Console is a class of the System namespace.
+// Console.WriteLine() will display any variable, string and every other data types.
+// A C# program can have multiple namespaces and classes.
+// Each namespace and class scope is surrounded by {}. Necessary codes are written within {} blocks.
+
+
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
