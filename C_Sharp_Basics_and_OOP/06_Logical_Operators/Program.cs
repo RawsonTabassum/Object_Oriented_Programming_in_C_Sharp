e79b9d8e35e1d2e19b53cf664f99dbd1a528bce8
RawsonTabassum <rawson17113@gmail.com>
2024-09-12 02:59:02 +0600

    Logical operators updated

--- C_Sharp_Basics_and_OOP/06_Logical_Operators/Program.cs
+++ C_Sharp_Basics_and_OOP/06_Logical_Operators/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// The logical operators supported by C# are &&(AND), ||(OR), and !(NOT)
+// Some comparison operators that produce boolean resuls are <, >, ==, <=, >= and !=
+// C# also supports some bitwise operators
+// &(bitwise-AND), |(bitwise-OR), ~(bitwise-NOT), << (left-shift), >> (right-shift) and >>> (unsigned right shift)
+// The following code skips the code example of the bitwise operators.
+
+// Logical operators are only applicable on boolean values and the resutl is also a boolean value.
+// Comparison operators can be applied on any data type and the result will be boolean value.
+
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = false;
+            bool b = true;
+
+            Console.WriteLine(a && b);
+            Console.WriteLine(a || b);
+            Console.WriteLine(!b);
+
+            int x = 10, y = 15, z = 10;
+            Console.WriteLine(x > y);
+            Console.WriteLine(x < y);
+            Console.WriteLine(x == z);
+            Console.WriteLine(x != y);
+            Console.WriteLine(y >= z);
+            Console.WriteLine(y <= z);
+        }
+    }
+}
