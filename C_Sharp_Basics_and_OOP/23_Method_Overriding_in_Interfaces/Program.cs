df1a6a50678a0b722dcd2016772365f304f027b2
RawsonTabassum <rawson17113@gmail.com>
2024-12-01 21:01:31 +0600

    Method overriding using interfaces updated

---
+++ C_Sharp_Basics_and_OOP/23_Method_Overriding_in_Interfaces/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Program{
+    class App{
+        public static void Main(string[] args){
+            Geometry circle = new Geometry("red", "green", 5);
+            Color c = circle;
+            Border b = circle;
+            c.fillColor("blue");
+            b.fillBorder("black", 10);
+            c.clear();
+            b.clear();
+        }
+    }
+
+    interface Color{
+        public void fillColor(string color);
+        public void clear();
+    }
+
+    interface Border{
+        public void fillBorder(string color, int px);
+        public void clear();
+    }
+
+    class Geometry: Color, Border{
+        public string color;
+        public string borderColor;
+        public int borderThickness;
+
+        public Geometry(string color, string borderColor, int borderThickness){
+            this.color = color;
+            this.borderColor = borderColor;
+            this.borderThickness = borderThickness;
+        }
+
+        void Color.fillColor(string color){
+            this.color = color;
+            Console.WriteLine($"The color changed to {color}");
+        }
+
+        void Color.clear(){
+            this.color = "white";
+            Console.WriteLine("The color is cleared");
+        }
+
+        void Border.fillBorder(string color, int px){
+            this.borderColor = color;
+            this.borderThickness = px;
+            Console.WriteLine($"The border color changed to {color} wiht {px}px thickness");
+        }
+
+        void Border.clear(){
+            this.borderColor = "white";
+            Console.WriteLine("The border is cleared");
+        }
+    }
+}
