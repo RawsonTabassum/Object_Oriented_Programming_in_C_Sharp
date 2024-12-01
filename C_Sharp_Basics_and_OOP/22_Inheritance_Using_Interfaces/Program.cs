ab6949a88a6f611383a6914255668f28ae68ed7f
RawsonTabassum <rawson17113@gmail.com>
2024-12-01 20:48:36 +0600

    Inheritance using interfaces updated

---
+++ C_Sharp_Basics_and_OOP/22_Inheritance_Using_Interfaces/Program.cs
@@ -0,0 +1,50 @@
+﻿// Interfaces are essentially a contract that enforces a set of methods that a class must implemet
+// They do not contain any code, only the declaration of methods
+// C# allows multiple inheritance because unlike classes, this does not cause ambiguity issues
+// Classes that inherit an interface must override the methods, otherwise produces error
+// Interface inheritance works similar to classes
+// While overriding methods, it is better to mention from which interface the method is coming from
+// We do this by using the .(dot) operator. 
+// For example, for interface Payment, override the method paypal with syntax Payment.paypal() 
+
+// While using overridden methods, first create the object of the class, then create object of the interface
+// Finally, assign the class object to the interface object.
+
+using System;
+
+namespace Interface{
+    class App{
+        public static void Main(string[] args){
+            Card c = new Card();
+            Paypal p = c;
+            p.payment();
+            Transaction t = c;
+            t.completeTransaction(1200);
+        }
+    }
+
+    interface Paypal{
+        public void payment();    // add a semicolon after function declaration.
+    }
+
+    interface Transaction{
+        public void completeTransaction(double balance);
+    }
+
+    class Card : Paypal, Transaction{       // multiple inheritance
+        protected double balance;
+        public Card(){
+            this.balance = 10000;
+        }
+
+        void Paypal.payment(){
+            Console.WriteLine("Making payment using paypal");
+        }
+
+        void Transaction.completeTransaction(double balance){
+            this.balance += balance;
+            Console.WriteLine("The transaction is complete");
+            Console.WriteLine($"New balance: {this.balance}");
+        }
+    }
+}
---
+++ C_Sharp_Basics_and_OOP/26_Shallow_vs_Deep_Copy/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace Copies{
+    class App{
+        public static void Main(){
+            ShallowCopy sc1 = new ShallowCopy(1, 2.3, "4");
+            ShallowCopy sc2 = sc1;
+
+            sc1.x = 10;
+            sc2.y = 20.3;
+            Console.WriteLine($"Value of x: {sc1.x} and {sc2.x}");
+            Console.WriteLine($"Value of y: {sc1.y} and {sc2.y}");
+
+            DeepCopy dc1 = new DeepCopy(5, 6.7, "8");
+            DeepCopy dc2 = new DeepCopy(dc1);
+            
+            dc1.x = 50;
+            dc2.y = 60.7;
+            Console.WriteLine($"Value of x: {dc1.x} and {dc2.x}");
+            Console.WriteLine($"Value of y: {dc1.y} and {dc2.y}");
+
+            int a = 0;
+            for(int i=0; i<=1000; i++){
+                a++;
+            }
+        }
+    }
+
+    class ShallowCopy{
+        public int x;
+        public double y;
+        public string z;
+
+        public ShallowCopy(int x, double y, string z){
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            Console.WriteLine("New Shallowcopy object created");
+        }
+
+        ~ShallowCopy(){
+            Console.WriteLine("Shallowcopy object is destroyed");
+        }
+    }
+
+    class DeepCopy{
+        public int x;
+        public double y;
+        public string z;
+
+        public DeepCopy(int x, double y, string z){
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            Console.WriteLine("New Deepcopy object created");
+        }
+
+        public DeepCopy(DeepCopy d){
+            this.x = d.x;
+            this.y = d.y;
+            this.z = d.z;
+            Console.WriteLine("New Deepcopy object created");
+        }
+
+        ~DeepCopy(){
+            Console.WriteLine("Deepcopy object is destroyed");
+        }
+    }
+}
