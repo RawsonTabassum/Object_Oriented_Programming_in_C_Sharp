6a93a9dee2c8f8901538723fe46a36d24fb44487
RawsonTabassum <rawson17113@gmail.com>
2024-12-01 22:41:16 +0600

    Overloading-overriding in the same method updated

---
+++ C_Sharp_Basics_and_OOP/28_Overloading_Overriding_of_Same_Method/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace Name
+{
+    class App{
+        public static void Main(string[] args){
+            Husky h = new Husky("Larry", 2, "Husky", 10);
+            h.display("Gene");
+            h.adopt();
+            h.display("Gene");
+        }
+    }
+
+    class Dog{
+        public string name;
+        public int age;
+
+        public Dog(string name, int age){
+            this.name = name;
+            this.age = age;
+        }
+
+        public virtual void display(){
+            Console.WriteLine("Dog information:");
+            Console.WriteLine($"Name: {this.name}, age: {this.age}");
+        }
+    }
+    
+    class Husky: Dog{
+        public string breed;
+        public double weight;
+        bool isAdopted = false;
+
+        public Husky(string name, int age, string breed, double weight): base(name, age){
+            this.breed = breed;
+            this.weight = weight;
+        }
+
+        public override void display(){
+            Console.WriteLine("Dog information:");
+            Console.WriteLine($"Name: {base.name}, Age: {base.age} years, Breed: {this.breed}, Weight: {this.weight} kg");
+        }
+
+        public void adopt(){
+            isAdopted = true;
+            Console.WriteLine($"You just adopted the {base.name}!");
+        }
+
+        public void display(string owner){
+            Console.WriteLine("Dog information:");
+            Console.WriteLine($"Name: {base.name}, Age: {base.age} years, Breed: {this.breed}, Weight: {this.weight}");
+            if(isAdopted){
+                Console.WriteLine($"{base.name}'s owner is {owner}");
+            } else {
+                Console.WriteLine($"{owner} has not adopted {base.name} yet");
+            }
+        }
+
+    }
+}
